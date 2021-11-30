b6ac252502b8479a0b11e44275fe651260df2128
Ethan Barnes <barnes.c.ethan@gmail.com>
2021-11-30 11:42:12 +0000

    Added fix to load large objects in (still problems rendering)

--- CameraControl/main.cpp
+++ CameraControl/main.cpp
@@ -118,16 +118,13 @@ int main(void)
 	};
 	// Read our .obj file
 	objl::Loader loader;
-	loader.LoadFile("cube.obj");
+	loader.LoadFile("cup.obj");
 
 	std::vector<GLfloat> vertices;
 	for (int i = 0; i < loader.LoadedVertices.size(); i++) {
 		vertices.push_back(loader.LoadedVertices[i].Position.X);
 		vertices.push_back(loader.LoadedVertices[i].Position.Y);
 		vertices.push_back(loader.LoadedVertices[i].Position.Z);
-		//vertices.push_back(0.7f);
-		//vertices.push_back(1.0f);
-		//vertices.push_back(0.1f);
 	}
 
 	std::vector<GLuint> indices;
@@ -144,11 +141,14 @@ int main(void)
     // ================================
     // buffer setup
     // ===============================
+
+
     glBindVertexArray(VAO);
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), &vertices[0], GL_STATIC_DRAW);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), &vertices[0], GL_STATIC_DRAW);
+
 	
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices[0], GL_STATIC_DRAW);
@@ -156,7 +156,7 @@ int main(void)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);	// Vertex attributes stay the same (change to 6 when adding color)
     glEnableVertexAttribArray(0);
 
-	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat))); // Color attribute
+	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat))); // Color attribute
 	//glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
